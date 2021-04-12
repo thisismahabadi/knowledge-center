5ce3f8c98ac1f8e4284297ffa4283c1d41248a23
Mohammad Mahabadi <itismahabadi@gmail.com>
2021-04-12 19:57:03 +0300

    Added database migration to tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+
+    /**
+     * Test initial setup.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->runDatabaseMigrations();
+    }
 }
